fd8ee1a6f25d0da24c9e0aaf36993ade46f6a0c2
Daniel Crosson <40742799+dcross42@users.noreply.github.com>
2024-03-24 16:34:55 -0400

    adjusted base class to make base url a required abstract property and api key rely on logic in authentication

--- gov_data/base.py
+++ gov_data/base.py
@@ -3,23 +3,26 @@ Base class for interacting with Government data APIs
 """
 
 from abc import ABC, abstractmethod
-from typing import Optional
+
+from .authentication import GovDataAPIKey
 
 
 class BaseGovDataAPI(ABC):
     """ Base class for interacting with Government data APIs """
-    def __init__(self, base_url: str, api_key : str | Optional[None] = None):
-        if api_key is not None:
-            self.api_key = api_key
-        self.base_url = base_url
+    def __init__(self, api_key : GovDataAPIKey):
+        if api_key is None:
+            raise ValueError("API key must be provided")      
+        self.api_key = api_key
+
+    @property
+    @abstractmethod
+    def base_url(self):
+        """ Base URL for the API """
 
     @abstractmethod
     def build_url(self, **kwargs):
         """ Build the URL to interact with the API """
-        pass
-    
+
     @abstractmethod
     def get_data(self, **kwargs):
         """ Get data from the API """
-        pass
-
