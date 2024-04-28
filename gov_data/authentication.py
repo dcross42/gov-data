8fdcbdcb03bb7652127ea660f2211966ee6a0820
Daniel Crosson <40742799+dcross42@users.noreply.github.com>
2024-04-28 16:39:21 -0400

    added pytest to the dependencies and added tests for authentication module

--- gov_data/authentication.py
+++ gov_data/authentication.py
@@ -5,15 +5,19 @@ import os
 
 class GovDataAPIKey:
     """ Handle import of API keys """
-    def __init__(self, api_name: str, api_key: str):
+    def __init__(self, api_name: str | None = None, api_key: str | None = None):
         # TODO: Add logic to ensure only implemented APIs are allowed
         if api_name is not None and api_key is None:
             env_var = f"GOVDATA_{api_name}_API_KEY"
-            self.api_key = os.environ[env_var]
-        elif api_name is None and api_key is not None:
+            self.api_key = os.getenv(env_var)
+            if self.api_key is None:
+                raise ValueError(f"Environment variable {env_var} not set")
+        # Case where both api_name and api_key are provided
+        elif api_key is not None:
             self.api_key = api_key
         else:
             raise ValueError("Either api_name or api_key must be provided")
+        
 
     def get_api_key(self):
         """ Get the API key """
