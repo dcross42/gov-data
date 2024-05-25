6a7d60f1a996c40ab05e0b200e3aa5f1db5b9314
Daniel Crosson <40742799+dcross42@users.noreply.github.com>
2024-05-25 18:01:59 -0400

    updated authentication function to only accept Fed api keys

--- gov_data/authentication.py
+++ gov_data/authentication.py
@@ -7,7 +7,7 @@ class GovDataAPIKey:
     """ Handle import of API keys """
     def __init__(self, api_name: str | None = None, api_key: str | None = None):
         # TODO: Add logic to ensure only implemented APIs are allowed
-        if api_name is not None and api_key is None:
+        if api_name is not None and api_name in ['FED'] and api_key is None:
             env_var = f"GOVDATA_{api_name}_API_KEY"
             self.api_key = os.getenv(env_var)
             if self.api_key is None:
@@ -17,7 +17,7 @@ class GovDataAPIKey:
             self.api_key = api_key
         else:
             raise ValueError("Either api_name or api_key must be provided")
-        
+
 
     def get_api_key(self):
         """ Get the API key """
